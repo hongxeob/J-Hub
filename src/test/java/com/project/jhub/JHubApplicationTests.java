34fc8ee4fbce4252955e55e1ef5932a4058ac13f
Hongseob Lee <sjun2918@naver.com>
2023-07-12 15:10:35 +0900

    chore : 프로젝트 초기 셋팅

---
+++ src/test/java/com/project/jhub/JHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
