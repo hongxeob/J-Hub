c431226a365fb10da276b1ca0c6d40dcc6bd2c58
Hongseob Lee <sjun2918@naver.com>
2023-07-12 17:39:51 +0900

    chore : Validate 의존성 추가, yml 작성

--- src/main/java/com/project/jhub/JHubApplication.java
+++ src/main/java/com/project/jhub/JHubApplication.java
@@ -2,8 +2,10 @@ package com.project.jhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JHubApplication {
 
     public static void main(String[] args) {
